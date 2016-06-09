94bec6b16359a8dc5e920869610db6b238b9935d
= <=>
2016-06-09 18:19:47 +0200

    View all nodes on netwerk

--- application/app/scripts/controllers/services.js
+++ application/app/scripts/controllers/services.js
@@ -2,9 +2,9 @@
 
 app.service('discoveryService', function ($http) {
 
-  this.discovery = function () {
+  this.discovery = function (callback) {
     $http.get('http://localhost:8081/discovery').then(function(data) {
-      console.log(data);
+      callback(data);
     });
   }
 
